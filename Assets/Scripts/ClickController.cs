e50f28f258b9c2193a753c282415b397146802db
Duncan <duncan.smale@gmail.com>
2019-07-18 17:35:12 +0200

    ClickController now takes into account objects collider bounds - uses 3d colliders.

--- Assets/Scripts/ClickController.cs
+++ Assets/Scripts/ClickController.cs
@@ -9,10 +9,14 @@ public class ClickController : MonoBehaviour
     private Vector3 position; //position of the object that is clicked on
     private Transform selected = null; //object that was clicked on
     [SerializeField] private LayerMask layerMask; //layermask to change which layer the ray cast will be performed on
+    [SerializeField] float aspectRatio;
+
+    float camSize;
 
     void Start()
     {
-        
+        camSize = Camera.main.orthographicSize; //the orthographic size of the camera
+        aspectRatio = Camera.main.aspect; //gets the camera property of the ratio, which is width divided by height
     }
 
     // Update is called once per frame
@@ -27,8 +31,13 @@ public class ClickController : MonoBehaviour
         if(selected != null)
         {
             Vector3 newPos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //takes the mouse position and transforms it into world co-ordinates.
-            newPos = new Vector3(Mathf.RoundToInt(newPos.x), Mathf.RoundToInt(newPos.y), 0);
-            selected.position = newPos;
+            Vector2 selectedBounds = selected.GetComponent<Collider>().bounds.extents;//gets the colliders bounds 
+            float offsetx = ((selectedBounds.x * 2) % 2) == 0 ? 0 : 0.5f; //sets the offsetx for odd bounded objects
+            float offsety = ((selectedBounds.y * 2) % 2) == 0 ? 0 : 0.5f; //sets offset y 
+            float x = Mathf.Clamp((Mathf.RoundToInt(newPos.x) + offsetx), -(int)(camSize * aspectRatio - selectedBounds.x) + 0.5f, (int)(camSize * aspectRatio - selectedBounds.x) - 0.5f); //creates a clamped value
+            float y = Mathf.Clamp((Mathf.RoundToInt(newPos.y) + offsety), -(camSize - selectedBounds.y), (camSize - selectedBounds.y)); //creates a clamped value
+            newPos = new Vector3(x, y, 0); //sets the objects transform in a new vector to make z value 0
+            selected.position = newPos; //sets objects postion
         }
     }
 
