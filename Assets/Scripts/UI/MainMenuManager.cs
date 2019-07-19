db49c5e26e9f2cae0cc5b6af90cb202f6200bad5
Keaton de Jager <keatondejager@gmail.com>
2019-07-19 13:38:27 +0200

    Sound effects and narrative on main menu complete, added functions to the buttons

---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+
+    public void StartGame() {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame () {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
